3bee983967c8f77ae1d70f3e10e3f225ae3e24f2
Darron Brown <DarronBrown88@outlook.com>
2018-06-26 12:47:26 -0600

    pushed alot of code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,64 @@
+'use strict';
+
 console.log('App.js is running');
 
-let template = <p>This is JSX from app.js</p>;
+var person = {
+	name: 'Darron Brown',
+	age: 30,
+	location: 'Salt Lake City',
+	sportsPlayed: ['Football', 'Basketball', 'Boxing']
+};
 
-ReactDOM.render(template);
+var templateTwo = React.createElement(
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		null,
+		person.name ? person.name : 'undefined'
+	),
+	console.log('Here is the name'),
+	person.sportsPlayed.length > 0 ? React.createElement(
+		'h3',
+		null,
+		'Here are your options'
+	) : React.createElement(
+		'h3',
+		null,
+		'There are no options'
+	),
+	React.createElement(
+		'ul',
+		null,
+		React.createElement(
+			'li',
+			null,
+			React.createElement(
+				'h2',
+				null,
+				person.age
+			)
+		),
+		React.createElement(
+			'li',
+			null,
+			React.createElement(
+				'h2',
+				null,
+				person.location != null && person.location
+			)
+		)
+	)
+);
+
+var verifyLocation = function verifyLocation(location) {
+	if (location != null) {
+		return location;
+	} else {
+		return 'Location does not exist';
+	}
+};
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(templateTwo, appRoot);

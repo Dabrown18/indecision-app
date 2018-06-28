551ed198443eaff1020922b6c9774094e4a56987
Darron Brown <DarronBrown88@outlook.com>
2018-06-28 17:31:52 -0600

    deleting specific option

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 // Stateless functional component
-
 class IndecisionApp extends React.Component {
 	constructor(props) {
 		super(props);
@@ -9,6 +8,7 @@ class IndecisionApp extends React.Component {
 		};
 
 		this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+		this.handleDeleteOption = this.handleDeleteOption.bind(this);
 		this.handlePick = this.handlePick.bind(this);
 		this.handleAddOption = this.handleAddOption.bind(this);
 	}
@@ -16,7 +16,13 @@ class IndecisionApp extends React.Component {
 	handleDeleteOptions() {
 	   this.setState(() => ({ options: []}));
    }
-g
+
+   handleDeleteOption(optionToRemove) {
+		this.setState((prevState) => ({
+			options: prevState.options.filter((option) => optionToRemove !== option)
+		}))
+	}
+
    handleAddOption(option) {
 
 		if(!option) {
@@ -48,6 +54,7 @@ g
         <Options
            options={this.state.options}
            handleDeleteOptions={this.handleDeleteOptions}
+			  handleDeleteOption={this.handleDeleteOption}
         />
         <AddOption
 			  handleAddOption={this.handleAddOption}
@@ -92,7 +99,14 @@ const Options = (props) => {
 		<div>
 			<button onClick={props.handleDeleteOptions}>Remove All</button>
 				{
-					props.options.map((option) => <Option key={option} optionText={option} />)
+					props.options.map((option) => (
+						<Option
+							key={option}
+							optionText={option}
+							handleDeleteOption={props.handleDeleteOption}
+
+						/>
+					))
 				}
 		</div>
 	);
@@ -102,6 +116,9 @@ const Option = (props) => {
 	return (
 		<div>
 			{props.optionText}
+			<button onClick={(e) => {
+				props.handleDeleteOption(props.optionText);
+			}}>remove</button>
 		</div>
 	)
 };

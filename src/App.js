99f92db2f7d99754fe84813d06c1a8f53855c970
Darron Brown <DarronBrown88@outlook.com>
2018-06-28 14:59:56 -0600

    refractored handleDeleteOptions

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class IndecisionApp extends React.Component {
 		super(props);
 
 		this.state = {
-		   options: []
+		   options: props.options
 		};
 
 		this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
@@ -14,13 +14,9 @@ class IndecisionApp extends React.Component {
 	}
 
 	handleDeleteOptions() {
-	   this.setState(() => {
-	   	return {
-	   	   options: []
-         }
-	   })
+	   this.setState(() => ({ options: []}));
    }
-
+g
    handleAddOption(option) {
 
 		if(!option) {
@@ -43,13 +39,12 @@ class IndecisionApp extends React.Component {
    }
 
 	render() {
-    const title = 'Indecision';
     const subtitle = 'Put your life in the hands of a computer';
     const options = ['Thing one', 'Thing two', 'Thing four'];
 
     return (
       <div>
-        <Header title={title} subtitle={subtitle} />
+        <Header subtitle={subtitle} />
         <Action
            hasOptions={this.state.options.length > 0}
            handlePick={this.handlePick}
@@ -66,13 +61,21 @@ class IndecisionApp extends React.Component {
   }
 }
 
+IndecisionApp.defaultProps = {
+	options: []
+};
+
 const Header = (props) => {
 	return (
 		<div>
 			<h1>{props.title}</h1>
-			<h2>{props.subtitle}</h2>
+			{props.subtitle && <h2>{props.subtitle}</h2>}
 		</div>
 	);
+};
+
+Header.defaultProps = {
+	title: 'Indecision'
 };
 
 const Action = (props) => {
